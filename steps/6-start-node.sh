f14c1c972abecaab5183762785ec996c368d87bd
Chaim Lev-Ari <chiptus@gmail.com>
2022-02-06 11:50:33 +0200

    fix(ledger): make ledger executable

--- steps/6-start-node.sh
+++ steps/6-start-node.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 function start_node() {
+    echo "starting node"
     sudo systemctl enable tezos-node
     sudo systemctl start tezos-node
     
+    echo "sleeping until node is ready"
     sleep 20
     
     tezos-client bootstrapped
