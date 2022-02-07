9857d34b8ead7bc33940826f5c83c61d170119c3
chiptus (rpi) <chiptus@gmail.com>
2022-02-07 15:00:41 +0000

    feat(ledger): import baker

--- import_ledger_baker.sh
+++ import_ledger_baker.sh
@@ -9,27 +9,29 @@ source ./utils/step_utils.sh
 DATA_DIR=./out
 
 function apply_udev() {
+    echo "downloading udev"
     FILENAME=$DATA_DIR/add_udev_rules.sh
     wget -O "$FILENAME" https://raw.githubusercontent.com/LedgerHQ/udev-rules/master/add_udev_rules.sh
     chmod +x "$FILENAME"
-    sudo $FILENAME
-    
-    restart_machine
+
+    echo "applying udev rules"
+    sudo $FILENAME   
 }
 
 function import_baker() {
-    import_cmd=$(tezos-client list connected ledgers | grep ed25519 | sed "s/ledger_$USER/$baker/g")
-    
-    $import_cmd
+    echo "importing ledger public key - please approve in ledger"
+    ledger_uri=$(tezos-client list connected ledgers | grep ed25519 | sed 's/.*\(ledger:.*\)"/\1/')
+    tezos-client import secret key baker "$ledger_uri"
 }
 
 function setup_ledger_to_baker() {
+    echo "approve ledger for baking:"
     tezos-client setup ledger to bake for "$baker"
 }
 
 baker=$1
 
-step apply_udev "udev"
+step apply_udev "udev" restart_machine
 step import_baker "import baker"
 step setup_ledger_to_baker "setup ledger to baker"
 
