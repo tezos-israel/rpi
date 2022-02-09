34a8dd7af0ef59f0c04e4ba0d07ff9f5a8efa38f
Chaim Lev-Ari <chiptus@gmail.com>
2022-02-09 16:10:44 +0200

    cache bakers details

--- utils/baker_details.sh
+++ utils/baker_details.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+details_file="$DATA_DIR"/baker_details
 
-echo "What's is the user name that runs tezos (default: $USER)"
-read -r user
-user=${user:-$USER}
+if [ -f "$details_file" ]; then
+    # shellcheck source=/dev/null
+    . "$details_file"
+fi
 
-echo "What's is your baker alias (default: baker)"
-read -r baker
-baker=${baker:-"baker"}
+if [ -z "$user" ]; then
+    echo "What's is the user name that runs tezos (default: $USER)"
+    read -r user
+    user=${user:-$USER}
+fi
+
+if [ -z "$baker" ]; then
+    echo "What's is your baker alias (default: baker)"
+    read -r baker
+    baker=${baker:-"baker"}
+fi
+
+cat > "$details_file" << EOF
+baker=$baker
+user=$user
+EOF
